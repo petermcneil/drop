d4bfe0a31572a7aa1e2204e1a595980666a0ef2e
petermcneil <petermcneil@users.noreply.github.com>
2019-02-04 15:53:17 +0000

    Initial commit for Drop app

---
+++ app/src/main/java/mcneil/peter/drop/activities/MapActivity.kt
@@ -0,0 +1,33 @@
+package mcneil.peter.drop.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import mcneil.peter.drop.R
+
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map)
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        val brighton = LatLng(50.8225, 0.1372)
+        mMap.addMarker(MarkerOptions().position(brighton).title("Marker in Brighton"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(brighton))
+    }
+}
